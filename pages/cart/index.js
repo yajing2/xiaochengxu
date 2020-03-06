a3b45a5668e572b49de7cd0a47040cea5c7f792f
yajing2 <yajing_du@icloud.com>
2020-03-06 22:17:30 +0800

    实现添加购物车功能

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,97 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 收货地址
+    address: {},
+    // 本地商品列表
+    goods: [],
+    // 总价格
+    allPrice: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    //  获取本地收货地址
+    this.setData({
+      // 如果本地没有address就是一个空的对象
+      address: wx.getStorageSync("address") || {}
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onShow() {
+    this.setData({
+      goods: wx.getStorageSync("goods") || []
+    });
+    // 计算总价格
+    this.handleAllPrice();
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 获取收货地址
+  handleGetAddress() {
+    wx.chooseAddress({
+      success: (res) => {
+        // 把收货地址保存到data
+        this.setData({
+          address: {
+            name: res.userName,
+            tel: res.telNumber,
+            detail: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+        // 保存到本地
+        wx.setStorageSync('address', this.data.address)
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 计算总价格
+  handleAllPrice() {
+    let price = 0;
+    this.data.goods.forEach(v => {
+      price += v.goods_price * v.number;
+    })
+    // 修改总价格
+    this.setData({
+      allPrice: price
+    })
+    // 修改本地数据
+    wx.setStorageSync("goods", this.data.goods)
   },
+  // 数量加1
+  handleCalc(e) {
+    // 点击的索引值
+    const {
+      index,
+      number
+    } = e.currentTarget.dataset;
+    this.data.goods[index].number += number;
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    // 判断如果数量为0时，用户是否删除
+    if (this.data.goods[index].number === 0) {
+      // 弹窗
+      wx.showModal({
+        title: '提示',
+        content: '是否删除商品',
+        success: (res) => {
+          // 确认删除
+          if (res.confirm) {
+            this.data.goods.splice(index, 1)
+          } else {
+            this.data.goods[index].number += 1;
+          }
+          //  重新修改data的goods值
+          this.setData({
+            goods: this.data.goods
+          })
+        }
+      })
+    }
+    this.setData({
+      goods: this.data.goods
+    })
 
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    //  计算总价
+    this.handleAllPrice()
   }
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
     //  商品的详情
-    detail:{},
+    detail: {},
     // 记录tab栏当前的索引
     current: 0,
     picUrls: []
@@ -15,7 +15,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     // 商品的详情
     request({
       url: "/goods/detail",
@@ -28,6 +28,7 @@ Page({
       } = res.data;
       // 获取图片的链接
       const picUrls = message.pics.map(v => {
+
         return v.pics_big
       });
       this.setData({
@@ -60,5 +61,43 @@ Page({
     wx.switchTab({
       url: '/pages/cart/index',
     })
+  },
+  // 把商品加入到本地的购物车列表
+  handleAddCart() {
+    // 每次加入商品之前，判断本地有没有数组，如果没有等于一个空数组
+    const goods = wx.getStorageSync("goods") || [];
+    // 判断当前的商品是否已经在goods的数组中,存在就加一，不存在就unshift
+    // some这个方法是循环数组，return的结果只有turn或者false
+    const exit = goods.some(v => {
+      const isExid = v.goods_id == this.data.detail.goods_id;
+      if (isExid) {
+        v.number += 1;
+
+        // 提示
+        wx.showToast({
+          title: '商品已存在',
+          icon: 'success',
+        })
+      }
+      return isExid;
+    })
+
+
+    if (!exit) {
+      goods.unshift({
+        goods_id: this.data.detail.goods_id,
+        goods_name: this.data.detail.goods_name,
+        goods_price: this.data.detail.goods_price,
+        goods_small_logo: this.data.detail.goods_small_logo,
+        number: 1
+      })
+      // 提示
+      wx.showToast({
+        title: '加入成功',
+        icon: 'success',
+      })
+    }
+    // 保存到本地
+    wx.setStorageSync("goods", goods)
   }
 })
